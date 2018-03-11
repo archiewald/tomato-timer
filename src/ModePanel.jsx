beaf265fce56dc1cafeb116b61725a5a69194a68
archiewald <artur.kozubek1@gmail.com>
2018-03-11 21:32:48 +0100

    add basic jest test, change ModePanel appearance bit

--- src/ModePanel.jsx
+++ src/ModePanel.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 
 export const ModePanel = () => (
-    <div className="container my-4">
-        <div className="row">
-            <div className="col-sm">
-                <button type="button" className="btn btn-secondary btn-lg btn-block">
-                    Pomodoro
-                </button> 
-            </div>
-            <div className="col-sm">
-                <button type="button" className="btn btn-secondary btn-lg btn-block">
-                    Short Break
-                </button> 
-            </div>
-            <div className="col-sm">
-                <button type="button" className="btn btn-secondary btn-lg btn-block">
-                    Long Break
-                </button> 
-            </div>
+    <div className="container my-4 text-center">
+        <div className="btn-group" role="group">
+            <button type="button" className="btn btn-secondary btn">
+                Pomodoro
+            </button> 
+            <button type="button" className="btn btn-secondary btn">
+                Short Break
+            </button> 
+            <button type="button" className="btn btn-secondary btn">
+                Long Break
+            </button> 
         </div>
     </div>
 )
