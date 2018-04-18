4f622b5375d0a1daba199a70700e2c52a6e9d2af
archiewald <artur.kozubek1@gmail.com>
2018-04-18 21:57:55 +0200

    delete comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,6 @@ export function setMode(mode){
 }
 
 export function startTimer(baseTime = 0){
-    console.log("in startTimer baseTime is ",baseTime);
     return {
         type: "START_TIMER",
         baseTime: baseTime,
